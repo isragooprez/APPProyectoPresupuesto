a3322ce100fac4ba773a68eb03a34a4c612540f9
ISRAGOO .PREZ <isragoo.prez@gmail.com>
2022-10-27 22:02:14 -0500

    issue1 Primer Commit Calculos Cabecero Totales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {IngresoServicio} from './ingreso/ingreso.servicio';
+import {EgresoServicio} from './egreso/egreso.servicio';
+import {Ingreso} from './ingreso/ingreso.model';
+import {Egreso} from './egreso/egreso.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'presupuesto-app';
+  ingresos: Ingreso[] = [];
+  egresos: Egreso[] = [];
+
+  constructor(private ingresoServicio: IngresoServicio, private egresoServicio: EgresoServicio) {
+    this.ingresos = ingresoServicio.ingresos;
+    this.egresos = egresoServicio.egresos;
+  }
+
+  getIngresosTotal(): number {
+    let ingresoTotal = 0;
+    this.ingresos.forEach(ingreso => {
+      ingresoTotal += ingreso.valor;
+    });
+    return ingresoTotal;
+  }
+
+
+  getEgresoTotal(): number {
+    let egresoTotal = 0;
+    this.egresos.forEach(egreso => {
+      egresoTotal += egreso.valor;
+    });
+    return egresoTotal;
+  }
+
+  getPorcentajeTotal(): number {
+    return this.getEgresoTotal() / this.getIngresosTotal();
+  }
+
+  getPresupuestoTotal(): number {
+    return this.getIngresosTotal() - this.getEgresoTotal();
+  }
+
 }
--- src/app/cabecero/cabecero.component.ts
+++ src/app/cabecero/cabecero.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-cabecero',
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CabeceroComponent implements OnInit {
 
-  constructor() { }
+  @Input() presupuestoTotal: number;
+  @Input() ingresoTotal: number;
+  @Input() egresoTotal: number;
+  @Input() porcentajeTotal: number;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
