509f37657dc9dff4a667f58c7382376ca0448256
ISRAGOO .PREZ <isragoo.prez@gmail.com>
2022-10-31 20:51:07 -0500

    issue1 Add Agresos

--- src/app/egreso/egreso.component.ts
+++ src/app/egreso/egreso.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {EgresoServicio} from './egreso.servicio';
+import {Egreso} from './egreso.model';
 
 @Component({
   selector: 'app-egreso',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EgresoComponent implements OnInit {
 
-  constructor() { }
+  egresos: Egreso[] = [];
+  @Input() ingresoTotal: number;
+
+  constructor(private egresoServicio: EgresoServicio) {
+  }
 
   ngOnInit(): void {
+    this.egresos = this.egresoServicio.egresos;
   }
+
+  eliminarEgreso(egreso: Egreso): void {
+    this.egresoServicio.eliminar(egreso);
+  }
+
+  calcularPorcentaje(egreso: Egreso): number {
+    return egreso.valor / this.ingresoTotal;
+  }
+
 
 }
